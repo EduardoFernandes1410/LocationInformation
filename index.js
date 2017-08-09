8fad35743551ea36d6250dbe9ac377a012851e07
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-08-09 13:38:45 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+/********************************SETUP**********************************/
+var http			= require('http');
+var fs				= require('fs');
+var url				= require('url');
+var path			= require('path');
+var express			= require('express');
+var mysql			= require('mysql');
+var bodyParser		= require('body-parser');
+var session			= require('express-session');
+var app 			= express();
+
+//*****MySQL*****//
+var connection;
+var db_config = {
+	host	: 'us-cdbr-iron-east-03.cleardb.net',
+	user	: 'bae2902327afa9',
+	password: 'aa09eff1',
+	database: 'heroku_c4f1d12a0ffd9dd'
+};
+
+//Conecta ao Banco de Dados
+function handleDisconnect() {
+	console.log('1. connecting to db:');
+	connection = mysql.createConnection(db_config);
+
+	connection.connect(function(err) {
+		if(err) {
+			console.log('2. error when connecting to db:', err);
+			setTimeout(handleDisconnect, 1000);
+		}
+	});
+
+	connection.on('error', function(err) {
+		console.log('3. db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+//Inicia DB
+handleDisconnect();
+
+//*****DEPENDENCIAS*****//
+//Utilizar o BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Utilizar o express-session
+app.use(session({secret: 'S3NH4'}));
+
+//Utilizar o express-static
+app.use(express.static('./', {
+	index: 'html/login/index.html'
+}));
+
+/*******************INICIO**********************/
+app.get("/", function(req, res) {
+	req.session.loginSucesso = false;
+	res.sendFile(path.join(__dirname, login));
+});
+
+/*************************INICIA SERVIDOR*****************************/
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+	console.log("Ouvindo na porta " + port);
+});
